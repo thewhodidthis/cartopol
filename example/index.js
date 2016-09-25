3810b446c4175b7730ce9fd4f4c53688a156613e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-25 22:14:54 +0200

    add example

---
+++ example/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const cartopol = require('../');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', input => {
+  let i = input.split(' ');
+  let x = parseInt(i[0]) || 0;
+  let y = parseInt(i[1]) || x;
+
+  console.log(cartopol(x, y));
+});
+
