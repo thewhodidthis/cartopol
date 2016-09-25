4c33167dfc1574665034fa3a4d33ab2951856a14
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-25 22:52:30 +0200

    rebuild

--- dist/cartopol.js
+++ dist/cartopol.js
@@ -2,6 +2,9 @@
 'use strict';
 
 module.exports = function cartopol(x, y) {
+  var x = parseInt(x, 10) || 0;
+  var y = parseInt(y, 10) || x;
+
   return {
     a: Math.atan2(y, x),
     d: Math.sqrt(x * x + y * y)
