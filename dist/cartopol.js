a51b6e3e6ab3192627087f46b71a6e2d962cb3a4
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-10-08 17:33:18 +0200

    rebuild

--- dist/cartopol.js
+++ dist/cartopol.js
@@ -2,12 +2,12 @@
 'use strict';
 
 module.exports = function cartopol(x, y) {
-  var x = parseInt(x, 10) || 0;
-  var y = parseInt(y, 10) || x;
+  var x = parseFloat(x) || 0;
+  var y = parseFloat(y) || 0;
 
   return {
-    a: Math.atan2(y, x),
-    d: Math.sqrt(x * x + y * y)
+    theta: Math.atan2(y, x),
+    r: Math.sqrt(x * x + y * y)
   };
 };
 
---
+++ test/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const test = require('tape');
+const cartopol = require('../');
+
+test('return value is of type and contains', (t) => {
+  t.plan(2);
+
+  t.equal(typeof cartopol(), 'object');
+  t.deepEqual(cartopol(0, 1), { r: 1, theta: Math.PI / 2 });
+});
+
+test('return value compares with known result', (t) => {
+  t.plan(2);
+
+  t.equals(cartopol().theta, 0);
+  t.equals(cartopol(1.5).r, Math.sqrt(2.25));
+});
+
