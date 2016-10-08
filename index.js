f32e79631e6efbe5df22c857ab4544d6257b5cf6
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-10-08 17:19:40 +0200

    allow float args, rename keys

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = function cartopol(x, y) {
-  var x = parseInt(x, 10) || 0;
-  var y = parseInt(y, 10) || x;
+  var x = parseFloat(x) || 0;
+  var y = parseFloat(y) || 0;
 
   return {
-    a: Math.atan2(y, x),
-    d: Math.sqrt(x * x + y * y)
+    theta: Math.atan2(y, x),
+    r: Math.sqrt(x * x + y * y)
   };
 };
