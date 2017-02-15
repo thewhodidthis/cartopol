286baf1087a31626278315cd552c7cde64c4be52
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-15 23:38:40 +0200

    amend test

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
 const test = require('tape');
-const cartopol = require('../');
+const car2pol = require('../');
 
 test('return value is of type and contains', (t) => {
   t.plan(2);
 
-  t.equal(typeof cartopol(), 'object');
-  t.deepEqual(cartopol(0, 1), { r: 1, theta: Math.PI / 2 });
+  t.equal(typeof car2pol(), 'object');
+  t.deepEqual(car2pol(0, 1), { r: 1, t: Math.PI / 2 });
 });
 
 test('return value compares with known result', (t) => {
   t.plan(2);
 
-  t.equals(cartopol().theta, 0);
-  t.equals(cartopol(1.5).r, Math.sqrt(2.25));
+  t.equals(car2pol(1.5).r, Math.sqrt(2.25));
+  t.equals(car2pol().t, 0);
 });
 
