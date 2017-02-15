3a194b4ddd2889a82e6cf79a8c6e13b492fc7de5
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-15 23:36:13 +0200

    rebuild

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-function cartopol() {
+var cartopol = function cartopol() {
   var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
   var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
   return {
-    theta: Math.atan2(y, x),
+    t: Math.atan2(y, x),
     r: Math.sqrt(x * x + y * y)
   };
-}
+};
 
 module.exports = cartopol;
