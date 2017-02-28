c38cb2d24b2a2217298ddbedbc1c590f61e3401a
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-28 20:04:00 +0200

    module: rebuild

--- dist/cartopol.js
+++ dist/cartopol.js
@@ -1,6 +1,15 @@
 var cartopol = (function () {
   'use strict';
 
+  /**
+   * Cartesian to polar util
+   * @module cartopol
+   * @param {Number} x - Across
+   * @param {Number} y - Up or down
+   * @returns {Object} - Vector
+   * @example
+   * cartopol(1, 1);
+   */
   var cartopol = function cartopol() {
     var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
     var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
