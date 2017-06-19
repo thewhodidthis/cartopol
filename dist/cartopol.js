5a1be6884dc717631153cea613b76714e94885e8
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-19 11:17:34 +0200

    module: rebuild per latest rollup config amends

--- dist/cartopol.js
+++ dist/cartopol.js
@@ -1,25 +1,24 @@
 var cartopol = (function () {
-  'use strict';
+'use strict';
 
-  /**
-   * Helps convert to polar
-   * @module cartopol
-   * @param {Number} x - Across
-   * @param {Number} y - Up or down
-   * @returns {Object} - Vector
-   * @example
-   * cartopol(1, 1);
-   */
-  var cartopol = function cartopol() {
-    var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-    var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-    return {
-      t: Math.atan2(y, x),
-      r: Math.sqrt(x * x + y * y)
-    };
+/**
+ * Helps convert to polar
+ * @module cartopol
+ * @param {Number} x - Across
+ * @param {Number} y - Up or down
+ * @returns {Object} - Vector
+ * @example
+ * cartopol(1, 1);
+ */
+var cartopol = function cartopol() {
+  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  return {
+    t: Math.atan2(y, x),
+    r: Math.sqrt(x * x + y * y)
   };
+};
 
-  return cartopol;
+return cartopol;
 
 }());
-//# sourceMappingURL=cartopol.js.map
