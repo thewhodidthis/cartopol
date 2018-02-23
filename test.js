e37dcfdd9e59b63e648f27805a98902749a89b87
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-23 11:59:27 +0100

    test: cleanup

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 'use strict'
 
 const { equal, ok, deepEqual, fail } = require('tapeless')
-const car2pol = require('./')
+const bender = require('./')
 
-equal(typeof car2pol(), 'object', 'should be equal', 'return value is of type and contains')
-deepEqual(car2pol(0, 1), { r: 1, t: Math.PI / 2 }, 'should be equivalent')
+equal(typeof bender(), 'object', 'should be equal', 'return value is of type and contains')
+deepEqual(bender(0, 1), { r: 1, t: Math.PI / 2 }, 'should be equivalent')
 
-const { r } = car2pol(1.5)
+const { r } = bender(1.5)
 
 equal(r, Math.sqrt(2.25), 'should be equal', 'return value compares with known result')
 
-const { t } = car2pol()
+const { t } = bender()
 
 equal(t, 0, 'should be equal')
 
 try {
-  car2pol(NaN, Infinity)
+  bender(NaN, Infinity)
   ok(true, 'should not throw', 'will not throw if misconfigured')
 } catch ({ message }) {
   fail(message)
